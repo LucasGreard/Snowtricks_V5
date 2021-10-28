e0c85efc3a664b1f3372280089c023360df79947
LucasGreard <86890031+LucasGreard@users.noreply.github.com>
2021-10-28 20:14:02 +0200

    Ajout systeme d'envoi de mail (sans token ni contenu)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Snowtricks_V5
+
 Project 6 : Snowtricks with Symfony 5 (V5)
+
+add composer require symfony/google-mailer (configure MAILER_DNS via .env)
+require PHP7
+require Symfony-CLI ?
+require PHPUnit
---
+++ src/Controller/MailerController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\USer;
+use App\Form\ForgotPasswordType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Flex\Path;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/mailer", name="mailer")
+     */
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $user = new USer();
+        $form = $this->createForm(ForgotPasswordType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userEmail = $request->request->get('forgot_password');
+            $this->sendEmail($userEmail['userEmail'], $mailer);
+        }
+
+
+        return $this->render('mailer/index.html.twig', [
+            'forgotPassword' => $form->createView(),
+        ]);
+    }
+
+    private function sendEmail($userEmail, MailerInterface $mailer)
+    {
+        $email = (new Email())
+            ->from('mestestemailoc@gmail.com')
+            ->to($userEmail)
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            // ->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+    }
+}
